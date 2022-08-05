981f0f95e547ec7bffbc6334030d893c5dbb7f47
Emmanuel Adedapo <thunadex1@gmail.com>
2022-08-05 17:54:13 +0100

    fix: packages name issues

--- packages/vue2/src/index.js
+++ packages/vue2/src/index.js
@@ -31,7 +31,7 @@ if (GlobalVue) {
 }
 
 // To allow use as module (npm/webpack/etc.) export component
-export default {Accordion, AccordionItem, Alert};  
+export {Accordion, AccordionItem, Alert};  
 
 // It's possible to expose named exports when writing components that can
 // also be used as directives, etc. - eg. import { RollupDemoDirective } from 'rollup-demo';
